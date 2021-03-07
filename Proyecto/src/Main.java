def8dfcfa4ac8a2a5e7eb0b421cb4af34a156a43
Marta <martamaleno.185@hotmail.com>
2021-03-07 17:42:02 +0100

    Cambio de código

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        String participantes[8] = {"Diego", "Edu", "Iván", "Javi", "Manuel", "Marta", "Pedro", "Roberto"};
         System.out.println("Inicio del programa.\n");
 
         System.out.println("Miembros del curso:");
+        for (int i=0; i<8; i++){
+            System.out.println(i+ ". " + participantes[i] + "\n");
+        }
 
         System.out.println("\nFinal del programa.");
     }
